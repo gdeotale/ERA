01e2495c381c5cdc78f615eba84b0ac7d3fcb4eb
Gunjan Deotale <gdeotale@gmail.com>
2023-06-03 00:20:09 +0530

    Adding files for session 5

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ERA
+
+Following Repository is part of ERA course under guidance of School of AI
+
+CHeckout different sessions for latest updates and implementations
---
+++ Session5/utils.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+def plot_batch(batch_data, batch_label):
+  fig = plt.figure()
+
+  for i in range(12):
+    plt.subplot(3,4,i+1)
+    plt.tight_layout()
+    plt.imshow(batch_data[i].squeeze(0), cmap='gray')
+    plt.title(batch_label[i].item())
+    plt.xticks([])
+    plt.yticks([])
+
+def plt_graph(train_losses,test_losses,train_acc,test_acc):
+  fig, axs = plt.subplots(2,2,figsize=(15,10))
+  axs[0, 0].plot(train_losses)
+  axs[0, 0].set_title("Training Loss")
+  axs[1, 0].plot(train_acc)
+  axs[1, 0].set_title("Training Accuracy")
+  axs[0, 1].plot(test_losses)
+  axs[0, 1].set_title("Test Loss")
+  axs[1, 1].plot(test_acc)
+  axs[1, 1].set_title("Test Accuracy")
