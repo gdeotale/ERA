14c0ecdc0c36c53365e8ad880903c60b30127809
Gunjan Deotale <gdeotale@gmail.com>
2023-07-01 10:11:45 +0530

    session 9

--- Session9/trainalbumentation.py
+++ Session9/trainalbumentation.py
@@ -2,20 +2,21 @@ import numpy as np
 from albumentations import *
 from albumentations.pytorch import ToTensorV2
 from albumentations.augmentations.dropout.coarse_dropout import CoarseDropout 
-
+from albumentations.augmentations.geometric.transforms import PadIfNeeded 
+from albumentations.augmentations.crops.transforms import CenterCrop
 class TrainAlbumentation():
   def __init__(self):
     self.train_transform = Compose([
-       HorizontalFlip(),
-       ColorJitter (brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2, always_apply=False, p=0.5),
-       CoarseDropout(max_holes = 1, max_height=16, max_width=16, min_holes = 1, min_height=16, min_width=16, fill_value=[255*0.485,255*0.456,255*0.406], mask_fill_value = None),
-       ShiftScaleRotate(shift_limit=(-0.2, 0.2), scale_limit=(-0.2, 0.2), rotate_limit=(-15, 15), p=0.5),
-       RGBShift (r_shift_limit=20, g_shift_limit=20, b_shift_limit=20, always_apply=False, p=0.5),
-       Normalize(
+      HorizontalFlip(),
+      ShiftScaleRotate(shift_limit=(-0.2, 0.2), scale_limit=(-0.2, 0.2), rotate_limit=(-15, 15), p=0.5),
+      PadIfNeeded(min_height=48, min_width=48, pad_height_divisor=None, pad_width_divisor=None, p=1.0),
+      CoarseDropout(max_holes = 1, max_height=16, max_width=16, min_holes = 1, min_height=16, min_width=16, fill_value=[255*0.485,255*0.456,255*0.406], mask_fill_value = None),
+      CenterCrop(32, 32, always_apply=False, p=1.0),
+      Normalize(
         mean=[0.485,0.456,0.406],
         std=[0.229,0.224,0.225],
-       ),
-       ToTensorV2()
+      ),
+      ToTensorV2()
     ])
 
   def __call__(self, img):
